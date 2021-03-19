8c52d9a4d8570c1f94ca2f0dc71b81fdd0413d2d
Bruno Igarzabal <brunoigar@live.com>
2021-03-19 16:05:56 -0300

    chore: add and configuring swagger

--- src/main/java/com/brunoIgarzabal/mmr/resources/DifficultyResource.java
+++ src/main/java/com/brunoIgarzabal/mmr/resources/DifficultyResource.java
@@ -3,6 +3,8 @@ package com.brunoIgarzabal.mmr.resources;
 import com.brunoIgarzabal.mmr.domain.Difficulty;
 import com.brunoIgarzabal.mmr.dto.DifficultyDTO;
 import com.brunoIgarzabal.mmr.services.DifficultyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -16,17 +18,20 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/difficulties")
+@Tag(name = "Difficulty", description = "Level of preparation of recipes API")
 public class DifficultyResource {
 
     @Autowired
     private DifficultyService service;
 
+    @Operation(summary = "Find difficulty by Id")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<Difficulty> find(@PathVariable Integer id) {
         Difficulty difficulty = service.find(id);
         return ResponseEntity.ok().body(difficulty);
     }
 
+    @Operation(summary = "Add a new difficulty")
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Void> insert(@Valid @RequestBody DifficultyDTO difficultyDto) {
         Difficulty difficulty = new Difficulty(difficultyDto.getId(), difficultyDto.getName(), difficultyDto.getLevel());
@@ -41,6 +46,7 @@ public class DifficultyResource {
         return ResponseEntity.created(uri).build();
     }
 
+    @Operation(summary = "Update an existing difficulty")
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Void> update(@Valid @RequestBody DifficultyDTO difficultyDto, @PathVariable Integer id) {
         Difficulty difficulty = new Difficulty(difficultyDto.getId(), difficultyDto.getName(), difficultyDto.getLevel());
@@ -49,12 +55,14 @@ public class DifficultyResource {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Delete a difficulty")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Find all difficulties")
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<DifficultyDTO>> findAll() {
         List<Difficulty> list = service.findAll();
@@ -67,6 +75,7 @@ public class DifficultyResource {
         return ResponseEntity.ok().body(listDto);
     }
 
+    @Operation(summary = "Pagination of difficulties")
     @RequestMapping(value = "/page",method = RequestMethod.GET)
     public ResponseEntity<Page<DifficultyDTO>> findPage(
             @RequestParam(value = "page", defaultValue = "0") Integer page,
